5ee24d4073d6dbc98a6ff1e7e5c63108d7259bfc
Shivam Goswami <shiv15101990@gmail.com>
2018-09-07 10:26:26 +1000

    Added invoice status in search api

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -115,7 +115,7 @@ class InvoicesController < ApplicationController
   def past_invoices
     if params[:search_term]
 
-      results = Invoice.search_by_company_customer_id(params[:search_term]).select(:id, :invoice_no, :user_id,
+      results = Invoice.search_by_company_customer_id(params[:search_term]).select(:id, :invoice_no, :user_id, :invoice_status,
                                                                                    :company_id, :customer_id, :invoice_date)
       render :json => results
     else
